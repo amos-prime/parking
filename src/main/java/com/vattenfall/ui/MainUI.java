4c207df758f486d880c7579f0d7aceca8a0d9a99
amoss <artur.mos@vattenfall.com>
2014-03-24 15:13:00 +0100

    some example data for vaadin view

--- src/main/java/com/vattenfall/ui/MainUI.java
+++ src/main/java/com/vattenfall/ui/MainUI.java
@@ -1,6 +1,8 @@
 package com.vattenfall.ui;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeSet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
@@ -8,8 +10,12 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vattenfall.model.DayState;
+import com.vattenfall.model.ParkingDay;
 import com.vattenfall.model.User;
+import com.vattenfall.model.UserStatus;
 import com.vattenfall.services.UserService;
+import org.joda.time.DateTime;
 
 /**
  * Main UI
@@ -19,7 +25,9 @@ public class MainUI extends UI {
 
     private static final long serialVersionUID = 1L;
 
-    /** Spring helper */
+    /**
+     * Spring helper
+     */
     private final SpringContextHelper helper;
 
     public MainUI() {
@@ -30,12 +38,30 @@ public class MainUI extends UI {
     protected void init(VaadinRequest request) {
         UserService userService = helper.getBean(UserService.class);
         List<User> allUsers = userService.findAll();
+        if (allUsers == null || allUsers.isEmpty()) {
+            allUsers = initData();
+            for (User user : allUsers) {
+                userService.create(user);
+            }
+        }
         VerticalLayout verticalLayout = new VerticalLayout();
         verticalLayout.setMargin(true);
         for (User user : allUsers) {
             verticalLayout.addComponent(new Label(user.getUsername()));
         }
         setContent(verticalLayout);
+    }
+
+    public List<User> initData() {
+        List<User> users = new LinkedList<User>();
+        for (int i = 1; i < 5; i++) {
+            User user = new User();
+            user.setUsername("User" + i);
+            user.setPassword("pass");
+            user.setStatus(UserStatus.HOLDER);
+            users.add(user);
+        }
+        return users;
     }
 
 }
